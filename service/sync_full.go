37bc0b5c64570f078636f425ad2595351ea9739e
wentaojin <1051866518@qq.com>
2021-12-25 23:09:57 +0800

    fix parition null

--- service/sync_full.go
+++ service/sync_full.go
@@ -219,6 +219,7 @@ func (e *Engine) InitWaitSyncTableMetaRecord(schemaName string, tableName []stri
 			SyncMode:         syncMode,
 			FullGlobalSCN:    -1,
 			FullSplitTimes:   -1,
+			IsPartition:      "NULL",
 		}).Error; err != nil {
 			return err
 		}
