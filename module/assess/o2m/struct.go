dc1e5cb91d13798b3f68871960fc79022aaef22c
wentaojin <1051866518@qq.com>
2022-12-12 11:36:36 +0800

    add assess structjson

--- module/assess/o2m/struct.go
+++ module/assess/o2m/struct.go
@@ -15,202 +15,340 @@ limitations under the License.
 */
 package o2m
 
+import "encoding/json"
+
 type ReportOverview struct {
-	ReportName        string
-	ReportUser        string
-	HostName          string
-	PlatformName      string
-	DBName            string
-	GlobalDBName      string
-	ClusterDB         string
-	ClusterDBInstance string
-	InstanceName      string
-	InstanceNumber    string
-	ThreadNumber      string
-	BlockSize         string
-	TotalUsedSize     string
-	HostCPUS          string
-	HostMem           string
-	CharacterSet      string
+	ReportName        string `json:"report_name"`
+	ReportUser        string `json:"report_user"`
+	HostName          string `json:"host_name"`
+	PlatformName      string `json:"platform_name"`
+	DBName            string `json:"db_name"`
+	GlobalDBName      string `json:"global_db_name"`
+	ClusterDB         string `json:"cluster_db"`
+	ClusterDBInstance string `json:"cluster_db_instance"`
+	InstanceName      string `json:"instance_name"`
+	InstanceNumber    string `json:"instance_number"`
+	ThreadNumber      string `json:"thread_number"`
+	BlockSize         string `json:"block_size"`
+	TotalUsedSize     string `json:"total_used_size"`
+	HostCPUS          string `json:"host_cpus"`
+	HostMem           string `json:"host_mem"`
+	CharacterSet      string `json:"character_set"`
 }
 
 type ReportSchema struct {
-	ListSchemaActiveSession               []ListSchemaActiveSession
-	ListSchemaTableSizeData               []ListSchemaTableSizeData
-	ListSchemaTableRowsTOP                []ListSchemaTableRowsTOP
-	ListSchemaTableObjectCounts           []ListSchemaTableObjectCounts
-	ListSchemaTablePartitionType          []ListSchemaTablePartitionType
-	ListSchemaTableColumnTypeAndMaxLength []ListSchemaTableColumnTypeAndMaxLength
-	ListSchemaTableAvgRowLength           []ListSchemaTableAvgRowLength
-	ListSchemaTemporaryTableCounts        []ListSchemaTemporaryTableCounts
+	ListSchemaActiveSession               []ListSchemaActiveSession               `json:"list_schema_active_session"`
+	ListSchemaTableSizeData               []ListSchemaTableSizeData               `json:"list_schema_table_size_data"`
+	ListSchemaTableRowsTOP                []ListSchemaTableRowsTOP                `json:"list_schema_table_rows_top"`
+	ListSchemaTableObjectCounts           []ListSchemaTableObjectCounts           `json:"list_schema_table_object_counts"`
+	ListSchemaTablePartitionType          []ListSchemaTablePartitionType          `json:"list_schema_table_partition_type"`
+	ListSchemaTableColumnTypeAndMaxLength []ListSchemaTableColumnTypeAndMaxLength `json:"list_schema_table_column_type_and_max_length"`
+	ListSchemaTableAvgRowLength           []ListSchemaTableAvgRowLength           `json:"list_schema_table_avg_row_length"`
+	ListSchemaTemporaryTableCounts        []ListSchemaTemporaryTableCounts        `json:"list_schema_temporary_table_counts"`
 }
 
 type ReportType struct {
-	ListSchemaIndexType      []ListSchemaIndexType
-	ListSchemaConstraintType []ListSchemaConstraintType
-	ListSchemaCodeType       []ListSchemaCodeType
-	ListSchemaSynonymType    []ListSchemaSynonymType
+	ListSchemaIndexType      []ListSchemaIndexType      `json:"list_schema_index_type"`
+	ListSchemaConstraintType []ListSchemaConstraintType `json:"list_schema_constraint_type"`
+	ListSchemaCodeType       []ListSchemaCodeType       `json:"list_schema_code_type"`
+	ListSchemaSynonymType    []ListSchemaSynonymType    `json:"list_schema_synonym_type"`
 }
 
 type ReportCheck struct {
-	ListSchemaPartitionTableCountsCheck  []ListSchemaPartitionTableCountsCheck
-	ListSchemaTableRowLengthCheck        []ListSchemaTableRowLengthCheck
-	ListSchemaTableIndexRowLengthCheck   []ListSchemaTableIndexRowLengthCheck
-	ListSchemaTableColumnCountsCheck     []ListSchemaTableAndIndexCountsCheck
-	ListSchemaIndexCountsCheck           []ListSchemaTableAndIndexCountsCheck
-	ListSchemaTableNumberTypeCheck       []ListSchemaTableNumberTypeCheck
-	ListUsernameLengthCheck              []ListUsernameLengthCheck
-	ListSchemaTableNameLengthCheck       []ListSchemaTableNameLengthCheck
-	ListSchemaTableColumnNameLengthCheck []ListSchemaTableColumnNameLengthCheck
-	ListSchemaTableIndexNameLengthCheck  []ListSchemaTableIndexNameLengthCheck
-	ListSchemaViewNameLengthCheck        []ListSchemaViewNameLengthCheck
-	ListSchemaSequenceNameLengthCheck    []ListSchemaSequenceNameLengthCheck
+	ListSchemaPartitionTableCountsCheck  []ListSchemaPartitionTableCountsCheck  `json:"list_schema_partition_table_counts_check"`
+	ListSchemaTableRowLengthCheck        []ListSchemaTableRowLengthCheck        `json:"list_schema_table_row_length_check"`
+	ListSchemaTableIndexRowLengthCheck   []ListSchemaTableIndexRowLengthCheck   `json:"list_schema_table_index_row_length_check"`
+	ListSchemaTableColumnCountsCheck     []ListSchemaTableAndIndexCountsCheck   `json:"list_schema_table_column_counts_check"`
+	ListSchemaIndexCountsCheck           []ListSchemaTableAndIndexCountsCheck   `json:"list_schema_index_counts_check"`
+	ListSchemaTableNumberTypeCheck       []ListSchemaTableNumberTypeCheck       `json:"list_schema_table_number_type_check"`
+	ListUsernameLengthCheck              []ListUsernameLengthCheck              `json:"list_username_length_check"`
+	ListSchemaTableNameLengthCheck       []ListSchemaTableNameLengthCheck       `json:"list_schema_table_name_length_check"`
+	ListSchemaTableColumnNameLengthCheck []ListSchemaTableColumnNameLengthCheck `json:"list_schema_table_column_name_length_check"`
+	ListSchemaTableIndexNameLengthCheck  []ListSchemaTableIndexNameLengthCheck  `json:"list_schema_table_index_name_length_check"`
+	ListSchemaViewNameLengthCheck        []ListSchemaViewNameLengthCheck        `json:"list_schema_view_name_length_check"`
+	ListSchemaSequenceNameLengthCheck    []ListSchemaSequenceNameLengthCheck    `json:"list_schema_sequence_name_length_check"`
 }
 
 type ListSchemaPartitionTableCountsCheck struct {
-	Schema          string
-	TableName       string
-	PartitionCounts string
+	Schema          string `json:"schema"`
+	TableName       string `json:"table_name"`
+	PartitionCounts string `json:"partition_counts"`
+}
+
+func (ro *ListSchemaPartitionTableCountsCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableRowLengthCheck struct {
-	Schema       string
-	TableName    string
-	AvgRowLength string
+	Schema       string `json:"schema"`
+	TableName    string `json:"table_name"`
+	AvgRowLength string `json:"avg_row_length"`
+}
+
+func (ro *ListSchemaTableRowLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableIndexRowLengthCheck struct {
-	Schema       string
-	TableName    string
-	IndexName    string
-	ColumnLength string
+	Schema       string `json:"schema"`
+	TableName    string `json:"table_name"`
+	IndexName    string `json:"index_name"`
+	ColumnLength string `json:"column_length"`
+}
+
+func (ro *ListSchemaTableIndexRowLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableAndIndexCountsCheck struct {
-	Schema    string
-	TableName string
-	Counts    string
+	Schema    string `json:"schema"`
+	TableName string `json:"table_name"`
+	Counts    string `json:"counts"`
+}
+
+func (ro *ListSchemaTableAndIndexCountsCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableNumberTypeCheck struct {
-	Schema        string
-	TableName     string
-	ColumnName    string
-	DataPrecision string
-	DataScale     string
+	Schema        string `json:"schema"`
+	TableName     string `json:"table_name"`
+	ColumnName    string `json:"column_name"`
+	DataPrecision string `json:"data_precision"`
+	DataScale     string `json:"data_scale"`
+}
+
+func (ro *ListSchemaTableNumberTypeCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListUsernameLengthCheck struct {
-	Schema        string
-	AccountStatus string
-	Created       string
+	Schema        string `json:"schema"`
+	AccountStatus string `json:"account_status"`
+	Created       string `json:"created"`
+}
+
+func (ro *ListUsernameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableNameLengthCheck struct {
-	Schema    string
-	TableName string
+	Schema    string `json:"schema"`
+	TableName string `json:"table_name"`
+}
+
+func (ro *ListSchemaTableNameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableColumnNameLengthCheck struct {
-	Schema     string
-	TableName  string
-	ColumnName string
+	Schema     string `json:"schema"`
+	TableName  string `json:"table_name"`
+	ColumnName string `json:"column_name"`
+}
+
+func (ro *ListSchemaTableColumnNameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableIndexNameLengthCheck struct {
-	Schema    string
-	TableName string
-	IndexName string
+	Schema    string `json:"schema"`
+	TableName string `json:"table_name"`
+	IndexName string `json:"index_name"`
+}
+
+func (ro *ListSchemaTableIndexNameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaViewNameLengthCheck struct {
-	Schema   string
-	ViewName string
-	ReadOnly string
+	Schema   string `json:"schema"`
+	ViewName string `json:"view_name"`
+	ReadOnly string `json:"read_only"`
+}
+
+func (ro *ListSchemaViewNameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaSequenceNameLengthCheck struct {
-	Schema       string
-	SequenceName string
-	OrderFlag    string
+	Schema       string `json:"schema"`
+	SequenceName string `json:"sequence_name"`
+	OrderFlag    string `json:"order_flag"`
+}
+
+func (ro *ListSchemaSequenceNameLengthCheck) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaIndexType struct {
-	Schema    string
-	IndexType string
-	Counts    string
+	Schema    string `json:"schema"`
+	IndexType string `json:"index_type"`
+	Counts    string `json:"counts"`
+}
+
+func (ro *ListSchemaIndexType) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaConstraintType struct {
-	Schema         string
-	ConstraintType string
-	Counts         string
+	Schema         string `json:"schema"`
+	ConstraintType string `json:"constraint_type"`
+	Counts         string `json:"counts"`
+}
+
+func (ro *ListSchemaConstraintType) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaCodeType struct {
-	Schema   string
-	CodeName string
-	CodeType string
-	Counts   string
+	Schema   string `json:"schema"`
+	CodeName string `json:"code_name"`
+	CodeType string `json:"code_type"`
+	Counts   string `json:"counts"`
+}
+
+func (ro *ListSchemaCodeType) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaSynonymType struct {
-	Schema     string
-	TableOwner string
-	Counts     string
+	Schema     string `json:"schema"`
+	TableOwner string `json:"table_owner"`
+	Counts     string `json:"counts"`
+}
+
+func (ro *ListSchemaSynonymType) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaActiveSession struct {
-	Rownum         string
-	DBID           string
-	InstanceNumber string
-	SampleID       string
-	SampleTime     string
-	SessionCounts  string
+	Rownum         string `json:"rownum"`
+	DBID           string `json:"dbid"`
+	InstanceNumber string `json:"instance_number"`
+	SampleID       string `json:"sample_id"`
+	SampleTime     string `json:"sample_time"`
+	SessionCounts  string `json:"session_counts"`
 }
+
+func (ro *ListSchemaActiveSession) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
+}
+
 type ListSchemaTableSizeData struct {
-	Schema        string
-	TableSize     string
-	IndexSize     string
-	LobTableSize  string
-	LobIndexSize  string
-	AllTablesRows string
+	Schema        string `json:"schema"`
+	TableSize     string `json:"table_size"`
+	IndexSize     string `json:"index_size"`
+	LobTableSize  string `json:"lob_table_size"`
+	LobIndexSize  string `json:"lob_index_size"`
+	AllTablesRows string `json:"all_tables_rows"`
+}
+
+func (ro *ListSchemaTableSizeData) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableRowsTOP struct {
-	Schema    string
-	TableName string
-	TableType string
-	TableSize string
+	Schema    string `json:"schema"`
+	TableName string `json:"table_name"`
+	TableType string `json:"table_type"`
+	TableSize string `json:"table_size"`
+}
+
+func (ro *ListSchemaTableRowsTOP) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableObjectCounts struct {
-	Schema     string
-	ObjectType string
-	Counts     string
+	Schema     string `json:"schema"`
+	ObjectType string `json:"object_type"`
+	Counts     string `json:"counts"`
+}
+
+func (ro *ListSchemaTableObjectCounts) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTablePartitionType struct {
-	Schema           string
-	TableName        string
-	PartitionType    string
-	SubPartitionType string
+	Schema           string `json:"schema"`
+	TableName        string `json:"table_name"`
+	PartitionType    string `json:"partition_type"`
+	SubPartitionType string `json:"sub_partition_type"`
+}
+
+func (ro *ListSchemaTablePartitionType) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableColumnTypeAndMaxLength struct {
-	Schema        string
-	DataType      string
-	Counts        string
-	MaxDataLength string
+	Schema        string `json:"schema"`
+	DataType      string `json:"data_type"`
+	Counts        string `json:"counts"`
+	MaxDataLength string `json:"max_data_length"`
+}
+
+func (ro *ListSchemaTableColumnTypeAndMaxLength) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTableAvgRowLength struct {
-	Schema       string
-	TableName    string
-	AvgRowLength string
+	Schema       string `json:"schema"`
+	TableName    string `json:"table_name"`
+	AvgRowLength string `json:"avg_row_length"`
+}
+
+func (ro *ListSchemaTableAvgRowLength) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
 }
 
 type ListSchemaTemporaryTableCounts struct {
-	Schema string
-	Counts string
+	Schema string `json:"schema"`
+	Counts string `json:"counts"`
+}
+
+func (ro *ListSchemaTemporaryTableCounts) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
+}
+
+func (ro *ReportOverview) String() string {
+	jsonStr, _ := json.Marshal(ro)
+	return string(jsonStr)
+}
+
+func (rs *ReportSchema) String() string {
+	jsonStr, _ := json.Marshal(rs)
+	return string(jsonStr)
+}
+
+func (rt *ReportType) String() string {
+	jsonStr, _ := json.Marshal(rt)
+	return string(jsonStr)
+}
+
+func (rc *ReportCheck) String() string {
+	jsonStr, _ := json.Marshal(rc)
+	return string(jsonStr)
 }
