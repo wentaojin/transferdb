67fe834ae3758503cfae83a247f420b5cfbae5ed
wentaojin <1051866518@qq.com>
2023-05-06 12:14:36 +0800

    fulltask error record

--- database/meta/chunk_error_detail.go
+++ database/meta/chunk_error_detail.go
@@ -20,21 +20,20 @@ import (
 	"fmt"
 	"github.com/wentaojin/transferdb/common"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type ChunkErrorDetail struct {
 	ID           uint   `gorm:"primary_key;autoIncrement;comment:'自增编号'" json:"id"`
-	DBTypeS      string `gorm:"type:varchar(30);index:idx_dbtype_st_map,unique;comment:'源数据库类型'" json:"db_type_s"`
-	DBTypeT      string `gorm:"type:varchar(30);index:idx_dbtype_st_map,unique;comment:'目标数据库类型'" json:"db_type_t"`
-	SchemaNameS  string `gorm:"type:varchar(100);not null;index:idx_dbtype_st_map,unique;comment:'源端 schema'" json:"schema_name_s"`
-	TableNameS   string `gorm:"type:varchar(100);not null;index:idx_dbtype_st_map,unique;comment:'源端表名'" json:"table_name_s"`
+	DBTypeS      string `gorm:"type:varchar(30);index:idx_dbtype_st_map;comment:'源数据库类型'" json:"db_type_s"`
+	DBTypeT      string `gorm:"type:varchar(30);index:idx_dbtype_st_map;comment:'目标数据库类型'" json:"db_type_t"`
+	SchemaNameS  string `gorm:"type:varchar(100);not null;index:idx_dbtype_st_map;comment:'源端 schema'" json:"schema_name_s"`
+	TableNameS   string `gorm:"type:varchar(100);not null;index:idx_dbtype_st_map;comment:'源端表名'" json:"table_name_s"`
 	SchemaNameT  string `gorm:"type:varchar(100);not null;comment:'目标端 schema'" json:"schema_name_t"`
 	TableNameT   string `gorm:"type:varchar(100);not null;comment:'目标端表名'" json:"table_name_t"`
-	TaskMode     string `gorm:"type:varchar(30);not null;index:idx_dbtype_st_map,unique;comment:'任务模式'" json:"task_mode"`
-	ChunkDetailS string `gorm:"type:varchar(300);not null;index:idx_dbtype_st_map,unique;comment:'表 chunk 切分信息'" json:"chunk_detail_s"`
+	TaskMode     string `gorm:"type:varchar(30);not null;index:idx_dbtype_st_map;comment:'任务模式'" json:"task_mode"`
+	ChunkDetailS string `gorm:"type:varchar(300);not null;index:idx_dbtype_st_map;comment:'表 chunk 切分信息'" json:"chunk_detail_s"`
 	InfoDetail   string `gorm:"type:longtext;not null;comment:'信息详情'" json:"info_detail"`
-	ErrorSQL     string `gorm:"type:longtext;not null;index:idx_dbtype_st_map,unique;comment:'错误 SQL'" json:"error_sql"`
+	ErrorSQL     string `gorm:"type:longtext;not null;comment:'错误 SQL'" json:"error_sql"`
 	ErrorDetail  string `gorm:"type:longtext;not null;comment:'错误详情'" json:"error_detail"`
 	*BaseModel
 }
@@ -76,17 +75,7 @@ func (rw *ChunkErrorDetail) CreateChunkErrorDetail(ctx context.Context, createS 
 	if err != nil {
 		return err
 	}
-	if err = rw.DB(ctx).Clauses(clause.OnConflict{
-		Columns: []clause.Column{
-			{Name: "db_type_s"},
-			{Name: "db_type_t"},
-			{Name: "schema_name_s"},
-			{Name: "table_name_s"},
-			{Name: "task_mode"},
-			{Name: "chunk_detail_s"},
-			{Name: "error_sql"}},
-		DoUpdates: clause.AssignmentColumns([]string{"schema_name_t", "table_name_t", "info_detail", "error_detail"}),
-	}).Create(createS).Error; err != nil {
+	if err = rw.DB(ctx).Create(createS).Error; err != nil {
 		return fmt.Errorf("create table [%s] record failed: %v", table, err)
 	}
 	return nil
